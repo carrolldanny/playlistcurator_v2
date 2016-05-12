fe82d547244ba32cc426e9b689958581ce8f0c95
dcarroll <dcarroll@tssg.org>
2016-05-12 15:43:52 +0100

    changing envs

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -14,15 +14,11 @@ module.exports = {
           8080,
 
   // MongoDB connection options
-  // mongo: {
-  //   //uri: 'mongodb://localhost/assignment2yeoman-dev'
-  //   uri: 'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz'
-  // }
   mongo: {
-    uri:  process.env.MONGOLAB_URI ||
+    uri:  process.env.process.env.MONGODB_URI ||
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz'
+          'mongodb://localhost/assignment2yeoman'
   }
 };
