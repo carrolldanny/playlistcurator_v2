baee01a36fd1e896a82d07b94fd461755095a98e
dcarroll <dcarroll@tssg.org>
2016-05-12 16:28:45 +0100

    changes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/assignment2yeoman-dev'
+    uri: 'mongodb://localhost/assignment2yeoman-dev' 
   },
 
   // Seed database on startup
