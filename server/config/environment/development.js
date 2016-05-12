ec47c8f3df73d85c2785511d1881d00b626ba859
dcarroll <dcarroll@tssg.org>
2016-05-12 17:15:53 +0100

    update

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz' 
+    uri: 'mongodb://00833827:ynnadc1@ds011492.mlab.com:11492/heroku_ds241z1d'
   },
 
   // Seed database on startup
