164a10bea417c514c9a44624d70a9989fae7ca7e
dcarroll <dcarroll@tssg.org>
2016-05-12 15:24:09 +0100

    changeing env vars

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,8 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/assignment2yeoman-dev'
+    //uri: 'mongodb://localhost/assignment2yeoman-dev'
+    uri: 'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz'
   },
 
   // Seed database on startup
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,10 +15,14 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:  process.env.MONGOLAB_URI ||
-          process.env.MONGOHQ_URL ||
-          process.env.OPENSHIFT_MONGODB_DB_URL +
-          process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz'
+    //uri: 'mongodb://localhost/assignment2yeoman-dev'
+    uri: 'mongodb://00833827:ynnadc1@ds011472.mlab.com:11472/heroku_v6dtznbz'
   }
+  // mongo: {
+  //   uri:  process.env.MONGOLAB_URI ||
+  //         process.env.MONGOHQ_URL ||
+  //         process.env.OPENSHIFT_MONGODB_DB_URL +
+  //         process.env.OPENSHIFT_APP_NAME ||
+  //         'mongodb://localhost/assignment2yeoman'
+  // }
 };
